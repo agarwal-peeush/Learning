993e477968145847e5e0dbd52fc21929b05faca5
Peeush Agarwal <agarwal.peeush@hotmail.com>
2019-04-26 11:22:39 +0530

    Add IExpenseReport, IExpenseApprover interfaces in ChainOfResponsibility Design pattern

---
+++ Learning-CSharp/Learning.DesignPatterns/ChainOfResponsibilityPattern/Interfaces.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.DesignPatterns.ChainOfResponsibilityPattern
+{
+    /// <summary>
+    /// ExpenseReport has total value of expense in Dollars/Cents
+    /// </summary>
+    public interface IExpenseReport
+    {
+        Decimal Total { get; }
+    }
+    /// <summary>
+    /// ExpenseApprover is an employee who can approve expense
+    /// </summary>
+    public interface IExpenseApprover
+    {
+        ApprovalResponse ApproveResponse(IExpenseReport expenseReport);
+    }
+
+    public enum ApprovalResponse
+    {
+        Denied, 
+        Approved,
+        BeyondApprovalLimit,
+    }
+}
