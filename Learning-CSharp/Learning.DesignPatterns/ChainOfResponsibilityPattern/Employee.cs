e08a76ec669202911cf4c4a74f4cc30a3f301b52
Peeush Agarwal <agarwal.peeush@hotmail.com>
2019-04-26 11:28:03 +0530

    Implemented IExpenseReport and IExpenseApprover

---
+++ Learning-CSharp/Learning.DesignPatterns/ChainOfResponsibilityPattern/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace Learning.DesignPatterns.ChainOfResponsibilityPattern
+{
+    public class Employee : IExpenseApprover
+    {
+        private readonly decimal _ApprovalLimit;
+
+        public Employee(string name, decimal approvalLimit)
+        {
+            Name = name;
+            _ApprovalLimit = approvalLimit;
+        }
+
+        public string Name { get; }
+
+        public ApprovalResponse ApproveResponse(IExpenseReport expenseReport)
+        {
+            return expenseReport.Total > _ApprovalLimit
+                ? ApprovalResponse.BeyondApprovalLimit
+                : ApprovalResponse.Approved;
+
+        }
+    }
+}
