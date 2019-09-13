da773eb4d24c6bf2a5ec8a69260ebadbe3e58a84
Peeush Agarwal <agarwal.peeush@hotmail.com>
2019-09-13 09:14:50 +0530

    Add BackgroudService and BackgroundTaskQueue to handle Background tasks from API controller

---
+++ Learning-CSharp/ApiWithBackgroundServices/ApiWithBackgroundServices/BackgroundServices/BackgroundTaskQueue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApiWithBackgroundServices.BackgroundServices
+{
+    public class BackgroundTaskQueue : IBackgroundTaskQueue
+    {
+        private ConcurrentQueue<Func<CancellationToken, Task>> _WorkItems =
+            new ConcurrentQueue<Func<CancellationToken, Task>>();
+        private SemaphoreSlim _Signal = new SemaphoreSlim(0);
+
+        public void QueueBackgroundWorkItem(
+            Func<CancellationToken, Task> workItem)
+        {
+            if (workItem == null)
+            {
+                throw new ArgumentNullException(nameof(workItem));
+            }
+
+            _WorkItems.Enqueue(workItem);
+            _Signal.Release();
+        }
+
+        public async Task<Func<CancellationToken, Task>> DequeueAsync(
+            CancellationToken cancellationToken)
+        {
+            await _Signal.WaitAsync(cancellationToken);
+            _WorkItems.TryDequeue(out var workItem);
+
+            return workItem;
+        }
+    }
+}
---
+++ Learning-CSharp/ApiWithBackgroundServices/ApiWithBackgroundServices/BackgroundServices/QueuedHostedService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApiWithBackgroundServices.BackgroundServices
+{
+    public class QueuedHostedService : BackgroundService
+    {
+        private readonly ILogger _Logger;
+
+        public QueuedHostedService(IBackgroundTaskQueue taskQueue, ILoggerFactory loggerFactory)
+        {
+            TaskQueue = taskQueue;
+            _Logger = loggerFactory.CreateLogger<QueuedHostedService>();
+        }
+
+        public IBackgroundTaskQueue TaskQueue { get; }
+
+        protected async override Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            _Logger.LogInformation("Queued Hosted Service is starting.");
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var workItem = await TaskQueue.DequeueAsync(cancellationToken);
+
+                try
+                {
+                    await workItem(cancellationToken);
+                }
+                catch (Exception ex)
+                {
+                    _Logger.LogError(ex,
+                       $"Error occurred executing {nameof(workItem)}.");
+                }
+            }
+
+            _Logger.LogInformation("Queued Hosted Service is stopping.");
+        }
+    }
+}
