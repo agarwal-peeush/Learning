e9c6db887f8cc55913d952fe218feab3014edfdc
Peeush Agarwal <agarwal.peeush@hotmail.com>
2019-11-08 10:33:52 +0530

    Demo work on gRPC using .NetCore

---
+++ DotNetCore3_0/GrpcService.Demo/GrpcService.Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService.Demo;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcService.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            //var response = await client.SayHelloAsync(new HelloRequest { Name = "World" });
+
+            //Console.WriteLine(response.Message);
+
+            var call = client.SayHelloStreaming(new HelloRequest { Name = "World Streaming" });
+
+            await foreach(var message in call.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine(message.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
