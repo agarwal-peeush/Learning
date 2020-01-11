d00f5456e1a2d30a1fe51499d1363f31c050f240
Peeush Agarwal <agarwal.peeush@hotmail.com>
2020-01-11 18:17:57 +0530

    Add comments for bettern explanation for Singleton pattern

--- Learning-CSharp/Learning.DesignPatterns/SingletonPattern/Logger.cs
+++ Learning-CSharp/Learning.DesignPatterns/SingletonPattern/Logger.cs
@@ -6,17 +6,21 @@ using System.Threading.Tasks;
 
 namespace Learning.DesignPatterns.SingletonPattern
 {
+    /* Sealed keyword restricts the inheritance */
     public sealed class Logger
     {
         private static Logger _Instance;
 
+        /* Private constructor ensures that object is not 
+         * instantiated other than with in the class itself. */
         private Logger()
         {
 
         }
 
+        /* Public property is used to return only one instance of the class leveraging on the private property */
         public static Logger Instance
-            => _Instance ?? (_Instance = new Logger());
+            => _Instance ?? (_Instance = new Logger()); //Lazy initialization - works well in Single-threaded environment.
 
         public void Log(string message)
         {
