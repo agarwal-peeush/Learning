6b874e7c0335a1fb080c4221d52f1113db79f713
Peeush Agarwal <agarwal.peeush@hotmail.com>
2020-01-11 18:07:00 +0530

    Singleton design pattern added

---
+++ Learning-CSharp/Learning.DesignPatterns/SingletonPattern/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.DesignPatterns.SingletonPattern
+{
+    public sealed class Logger
+    {
+        private static Logger _Instance;
+
+        private Logger()
+        {
+
+        }
+
+        public static Logger Instance
+            => _Instance ?? (_Instance = new Logger());
+
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
